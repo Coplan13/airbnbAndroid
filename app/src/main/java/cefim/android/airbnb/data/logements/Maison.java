e20761ace06c70cc6a1268684f59546b4f328a14
Coplan <Coplan@MBP-de-Coplan.pedago>
2019-04-10 12:12:09 +0200

    first commit

---
+++ app/src/main/java/cefim/android/airbnb/data/logements/Maison.java
@@ -0,0 +1,54 @@
+package cefim.android.airbnb.data.logements;
+
+import cefim.android.airbnb.data.utilisateurs.Hote;
+
+/**
+ * Created by Coplan on 10/04/2019.
+ */
+
+
+
+
+public final class Maison extends Logement {
+
+    private final int superficieJardin;
+    private final boolean possedePiscine;
+
+    public Maison(Hote hote, int tarifParNuit, String adresse, int superficie, int nbVoyageursMax, int superficieJardin, boolean possedePiscine) {
+        super(hote, tarifParNuit, adresse, superficie, nbVoyageursMax);
+
+        this.superficieJardin = superficieJardin;
+        this.possedePiscine = possedePiscine;
+    }
+
+
+    @Override
+    public void afficher() {
+
+        super.afficher();
+        System.out.println("Le logement est une maison située " + getAdresse() + " pour " + getNbVoyageursMax() + " utilisateurs max.");
+        System.out.println("Superficie : " + getSuperficie() + "m2");
+
+        if (superficieJardin > 0) {
+            System.out.println("Jardin : " + superficieJardin + "m2");
+        } else {
+            System.out.println("Jardin : non");
+        }
+
+        // System.out.println("Piscine : " + ((possedePiscine)? "oui" : "non"));
+
+        if (possedePiscine) {
+            System.out.println("Piscine : oui");
+        } else {
+            System.out.println("Piscine : non");
+        }
+    }
+
+    public int getSuperficieJardin() {
+        return superficieJardin;
+    }
+
+    public boolean getPossedePiscine() {
+        return possedePiscine;
+    }
+}
---
+++ app/src/main/java/cefim/android/airbnb/data/utilisateurs/Voyageur.java
@@ -0,0 +1,14 @@
+package cefim.android.airbnb.data.utilisateurs;
+
+/**
+ * Created by Coplan on 10/04/2019.
+ */
+
+public final class Voyageur extends Personne {
+
+    public Voyageur(String pNom, String pPrenom, int pAge) {
+        super(pNom, pPrenom, pAge);
+    }
+
+}
+

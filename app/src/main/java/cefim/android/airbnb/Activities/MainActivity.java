8ad6f232fdd23d66889896778a68f60dde7587ee
Mehdi <mehdi.serra57@gmail.com>
2019-04-11 16:34:50 +0200

    Bottom Navigation Bar créé

--- app/src/main/java/cefim/android/airbnb/Activities/MainActivity.java
+++ app/src/main/java/cefim/android/airbnb/Activities/MainActivity.java
@@ -1,7 +1,11 @@
 package cefim.android.airbnb.Activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import cefim.android.airbnb.R;
 
@@ -11,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_explorer:
+                        Toast.makeText(MainActivity.this, "Explorer", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_favoris:
+                        Toast.makeText(MainActivity.this, "Favoris", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profil:
+                        Toast.makeText(MainActivity.this, "Profil", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
